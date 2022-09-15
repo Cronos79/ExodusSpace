b0cc1d391d36bd1bd1b2531711e5ca3c0f81683f
MattB79 <cronos1979@live.com>
2022-09-14 20:18:54 -0500

    Add character movement

--- Source/ExodusSpace/Character/Player/ESPlayerCharacter.cpp
+++ Source/ExodusSpace/Character/Player/ESPlayerCharacter.cpp
@@ -2,13 +2,44 @@
 
 
 #include "ESPlayerCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include <Camera/CameraComponent.h>
 
 AESPlayerCharacter::AESPlayerCharacter()
 {
-
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	SpringArmComp->SetupAttachment(RootComponent);
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("Main Camera"));
+	CameraComp->SetupAttachment(SpringArmComp, USpringArmComponent::SocketName);
+	
 }
 
 void AESPlayerCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AESPlayerCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AESPlayerCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("Turn", this, &AESPlayerCharacter::Turn);
+	PlayerInputComponent->BindAxis("LookUp", this, &AESPlayerCharacter::LookUp);
+}
+
+void AESPlayerCharacter::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector(), Value);
+}
+
+void AESPlayerCharacter::MoveRight(float Value)
+{
+	AddMovementInput(GetActorRightVector(), Value);
+}
+
+void AESPlayerCharacter::Turn(float Value)
+{
+	AddControllerYawInput(Value);
+}
+
+void AESPlayerCharacter::LookUp(float Value)
+{
+	AddControllerPitchInput(-Value);
 }
--- Source/ExodusSpace/Character/Player/ESPlayerCharacter.h
+++ Source/ExodusSpace/Character/Player/ESPlayerCharacter.h
@@ -6,6 +6,9 @@
 #include "../ESCharacter.h"
 #include "ESPlayerCharacter.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
+
 /**
  * 
  */
@@ -14,8 +17,23 @@ class EXODUSSPACE_API AESPlayerCharacter : public AESCharacter
 {
 	GENERATED_BODY()
 
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USpringArmComponent* SpringArmComp;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UCameraComponent* CameraComp;
+
 public:
 	AESPlayerCharacter();
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION()
+	void MoveForward(float Value);
+	UFUNCTION()
+	void MoveRight(float Value);
+	UFUNCTION()
+	void Turn(float Value);
+	UFUNCTION()
+	void LookUp(float Value);
 };
